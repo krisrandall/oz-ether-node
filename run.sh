261437a2faaf2beb45cca2b71323b8fcb3e35844
krisrandall <kris.randall@gmail.com>
2017-10-18 16:19:16 +1000

    fix bootnode string

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ BOOT_RPC_PORT=8555
 # obtain enode (public key) of bootnode
 BOOT_INODE=$(curl -sX POST --data '{"jsonrpc":"2.0","method":"admin_nodeInfo", "id":1}' http://${BOOT_SERVER_IP}:${BOOT_RPC_PORT})
 BOOT_INODE=$(expr "${BOOT_INODE}" : '.*"id":"\([^"]*\)"') 
-BOOT_INODE=${BOOT_INODE}@[${BOOT_SERVER_IP}]:${BOOT_RPC_PORT}
+BOOT_INODE=enode://${BOOT_INODE}@[${BOOT_SERVER_IP}]:${BOOT_RPC_PORT}
 
 docker run -it -d -v ${PWD}/blockchain_node:/root \
      -p ${BOOT_RPC_PORT}:${BOOT_RPC_PORT} -p ${NETWORK_PORT}:${NETWORK_PORT}  \
      ethereum/client-go --port ${NETWORK_PORT} --networkid 4552 --rpc --rpcport ${BOOT_RPC_PORT} \
      --datadir /root --verbosity 3 --rpcaddr 0.0.0.0 \
      --rpcapi eth,net,web3,personal,admin \
-     --bootnodes {BOOT_INODE} \
+     --bootnodes ${BOOT_INODE} \
      console
