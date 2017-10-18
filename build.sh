04b33cb132bc2a695c85fad471c570f734928d47
krisrandall <kris.randall@gmail.com>
2017-10-18 11:36:12 +1000

    Wipe all earlier attempts ... ready for the new

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t oz-ether-node .
--- run.sh
+++
@@ -1,8 +0,0 @@
-
-docker kill oz-ether-node-inst
-docker rm oz-ether-node-inst
-
-sudo docker run -p 9000:9000 --name oz-ether-node-inst -t -i oz-ether-node 
-
-
-
