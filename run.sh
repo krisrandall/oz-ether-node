5f0a0076c458612ad66e0dce2c3e721054e639c8
krisrandall <kris.randall@gmail.com>
2017-10-20 10:31:00 +1000

    add correct port for bootnodes enode string,  change inode to correct enode

--- run.sh
+++ run.sh
@@ -8,14 +8,14 @@ BOOT_SERVER_IP=104.131.122.94
 BOOT_RPC_PORT=8555
 
 # obtain enode (public key) of bootnode
-BOOT_INODE=$(curl -sX POST --data '{"jsonrpc":"2.0","method":"admin_nodeInfo", "id":1}' http://${BOOT_SERVER_IP}:${BOOT_RPC_PORT})
-BOOT_INODE=$(expr "${BOOT_INODE}" : '.*"id":"\([^"]*\)"') 
-BOOT_INODE=enode://${BOOT_INODE}@[${BOOT_SERVER_IP}]:${BOOT_RPC_PORT}
+BOOT_ENODE=$(curl -sX POST --data '{"jsonrpc":"2.0","method":"admin_nodeInfo", "id":1}' http://${BOOT_SERVER_IP}:${BOOT_RPC_PORT})
+BOOT_ENODE=$(expr "${BOOT_ENODE}" : '.*"id":"\([^"]*\)"') 
+BOOT_ENODE=enode://${BOOT_ENODE}@[${BOOT_SERVER_IP}]:${NETWORK_PORT}
 
 docker run -it -d -v ${PWD}/blockchain_node:/root \
      -p ${BOOT_RPC_PORT}:${BOOT_RPC_PORT} -p ${NETWORK_PORT}:${NETWORK_PORT}  \
      ethereum/client-go --port ${NETWORK_PORT} --networkid 4552 --rpc --rpcport ${BOOT_RPC_PORT} \
      --datadir /root --verbosity 3 --rpcaddr 0.0.0.0 \
      --rpcapi eth,net,web3,personal,admin \
-     --bootnodes ${BOOT_INODE} \
+     --bootnodes ${BOOT_ENODE} \
      console
